ee60f215338e846f455013504fae98a87feeb112
Anupama98 <anubalasooriya2017@gmail.com>
2020-11-04 00:07:59 +0530

    Complete Base class and TestData class

--- src/test/java/com/pragmatic/addEmployee/Base.java
+++ src/test/java/com/pragmatic/addEmployee/Base.java
@@ -1,4 +1,38 @@
 package com.pragmatic.addEmployee;
 
+import com.pragmatic.util.TestData;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+
 public class Base {
+    public static WebDriver driver;
+    public TestData data;
+
+    @BeforeClass
+    public void beforeClass() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver.get(data.url);
+
+        // Login to the site
+        driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys(data.username);
+        driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys(data.password);
+        driver.findElement(By.xpath("//input[@id='btnLogin']")).click();
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
 }
--- src/test/java/com/pragmatic/util/TestData.java
+++ src/test/java/com/pragmatic/util/TestData.java
@@ -1,4 +1,7 @@
 package com.pragmatic.util;
 
 public class TestData {
+    public String url = "http://hrm.pragmatictestlabs.com/";
+    public String username = "Admin";
+    public String password = "Ptl@#321";
 }
